a91e59b3eb43cc4a1668080e84094cd459ca5490
xishengbo <xishengbo@baidu.com>
2023-11-26 22:25:59 +0800

    23-11-26 更新插件图片

--- webpack.config.js
+++ webpack.config.js
@@ -169,16 +169,7 @@ var options = {
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: 'src/assets/img/icon-128.png',
-          to: path.join(__dirname, 'build'),
-          force: true,
-        },
-      ],
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: 'src/assets/img/icon-34.png',
+          from: 'src/assets/img/wxread-icon.png',
           to: path.join(__dirname, 'build'),
           force: true,
         },
